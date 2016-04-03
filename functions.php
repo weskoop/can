23082065dde631470eb6d23f973501b562c8fb34
Wes Koopmans <wes@ofdesks.com>
2016-04-03 15:27:01 -0700

    Can 1.0

--- functions.php
+++ functions.php
@@ -5,16 +5,19 @@
  * @package can
  */
 
-// Scripts
-function can_scripts() {
-	wp_enqueue_style( 'can-style', get_template_directory_uri() . '/style.css' );
-}
-add_action( 'wp_enqueue_scripts', 'can_scripts' );
+// Title Tag
+add_theme_support( 'title-tag' );
+
+// Gallery Styles Suck
+add_filter( 'use_default_gallery_style', '__return_false' );
 
 // Nav Menu
 register_nav_menus( array(
 	'primary'  => __( 'Header', 'can' )
 ) );
 
-// Gallery Styles Suck
-add_filter( 'use_default_gallery_style', '__return_false' );
+// Scripts
+function can_scripts() {
+	wp_enqueue_style( 'can-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'can_scripts', 9 );
